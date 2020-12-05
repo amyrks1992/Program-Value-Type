067b616cc0bd0d29224caadf539df7c1a08743f8
Amyr Kamel e Silva <67295939+amyrks1992@users.noreply.github.com>
2020-12-05 19:05:55 -0300

    Add files via upload

---
+++ Program_Type_Value.py
@@ -0,0 +1,17 @@
+x = int (input('Digite um número: ')) 
+
+def valorInteiro (x): 
+
+ if x > 0: 
+
+  return 1 
+
+ elif x < 0: 
+
+  return - 1 
+
+ elif x == 0: 
+
+  return 0 
+
+print (valorInteiro (x)) 
